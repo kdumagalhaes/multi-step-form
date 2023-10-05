7c1bd1e244a5080ad38bc8bc4f08e5c193a4aeae
Carlos Pereira <carlos.pereira@tekever.com>
2023-10-05 17:59:09 +0100

    Add Step component

---
+++ src/components/Step/Step.tsx
@@ -0,0 +1,20 @@
+import styles from './Step.module.scss';
+
+interface StepProps {
+  stepNumber: string
+  stepInfo: string
+  isActive?: boolean
+}
+const Step = ({stepNumber, stepInfo, isActive = false}: StepProps) => {
+  return (
+    <div className={styles.step}>
+      <div className={isActive ? `${styles.number} ${styles["number-active"]}` : styles.number}>{stepNumber}</div>
+      <div className={styles["text-container"]}>
+      <p className={styles.indication}>step {stepNumber}</p>
+      <p className={styles.info}>{stepInfo}</p>
+      </div>
+    </div>
+  )
+};
+
+export default Step;
