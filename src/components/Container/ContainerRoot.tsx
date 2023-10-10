8d4c1aa0db5b07c32025f7008bbaf9f9111c20fe
Carlos Pereira <carlos.pereira@tekever.com>
2023-10-10 22:07:18 +0100

    Add Container components

---
+++ src/components/Container/ContainerRoot.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+import styles from "./ContainerRoot.module.scss"
+
+interface ContainerRootProps {
+  children: ReactNode
+}
+const ContainerRoot = ({children}: ContainerRootProps) => {
+  return (
+    <div className={styles.container}>
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default ContainerRoot
---
+++ src/components/Container/index.tsx
@@ -0,0 +1,9 @@
+import ContainerLeft from './ContainerLeft';
+import ContainerRight from './ContainerRight';
+import ContainerRootProps from './ContainerRoot';
+
+export const Container = {
+  Root: ContainerRootProps,
+  Left: ContainerLeft,
+  Right: ContainerRight
+};
