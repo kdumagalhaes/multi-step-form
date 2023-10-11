b275b3b57a74f72db37aaf97ed9f82b6ca162b0d
Carlos Pereira <carlos.pereira@tekever.com>
2023-10-11 22:37:17 +0100

    Add paths constant

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,15 +4,16 @@ import UserInfo from '../pages/UserInfo';
 import SelectPlan from '../pages/SelectPlan';
 import AddOns from '../pages/AddOns';
 import Summary from '../pages/Summary/Summary';
+import { Path } from '../constants/pages';
 export function Router() {
   return (
     <Routes>
       <Route path='/' element={<DefaultLayout />}>
         <Route path='/' element={<UserInfo />} />
-        <Route path='/user-info' element={<UserInfo />} />
-        <Route path='/select-plan' element={<SelectPlan />} />
-        <Route path='/add-ons' element={<AddOns />} />
-        <Route path='/summary' element={<Summary />} />
+        <Route path={Path.USER_INFO} element={<UserInfo />} />
+        <Route path={Path.SELECT_PLAN} element={<SelectPlan />} />
+        <Route path={Path.ADD_ONS} element={<AddOns />} />
+        <Route path={Path.SUMMARY} element={<Summary />} />
       </Route>
     </Routes>
   );
