2357dc37eb6add84b99e8c86372eb2f56c6c79ef
Carlos Pereira <carlos.pereira@tekever.com>
2023-10-13 19:56:59 +0100

    Add basic select plan cards structure

--- src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
+++ src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
@@ -1,5 +1,12 @@
+import styles from "./ContentSelectPlan.module.scss"
 const ContentSelectPlan = () => {
-  return <div>select plan</div>;
+  return (
+    <div className={styles.container}>
+    <div role="button" className={styles['plan-card']}>select plan</div>
+    <div role="button" className={styles['plan-card']}>select plan</div>
+    <div role="button" className={styles['plan-card']}>select plan</div>
+    </div>
+  );
 };
 
 export default ContentSelectPlan;
