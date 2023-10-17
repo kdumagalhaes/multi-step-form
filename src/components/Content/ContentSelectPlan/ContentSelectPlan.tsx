fd58d1ab35ba00be8e6e788829c5fbdbb270b200
Carlos Pereira <carlos.pereira@tekever.com>
2023-10-17 21:50:09 +0100

    Add icons in select plan step

--- src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
+++ src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
@@ -1,10 +1,23 @@
-import styles from "./ContentSelectPlan.module.scss"
+import IconAdvanced from '../../../assets/icons/icon-advanced.svg';
+import IconArcade from '../../../assets/icons/icon-arcade.svg';
+import IconPro from '../../../assets/icons/icon-pro.svg';
+
+import styles from './ContentSelectPlan.module.scss';
 const ContentSelectPlan = () => {
   return (
     <div className={styles.container}>
-    <div role="button" className={styles['plan-card']}>select plan</div>
-    <div role="button" className={styles['plan-card']}>select plan</div>
-    <div role="button" className={styles['plan-card']}>select plan</div>
+      <div role='button' className={styles['plan-card']}>
+        <img src={IconArcade} alt='arcade icon' />
+        select plan
+      </div>
+      <div role='button' className={styles['plan-card']}>
+        <img src={IconAdvanced} alt='advanced icon' />
+        select plan
+      </div>
+      <div role='button' className={styles['plan-card']}>
+        <img src={IconPro} alt='pro icon' />
+        select plan
+      </div>
     </div>
   );
 };
