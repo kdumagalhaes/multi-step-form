f3eee937cd9979f0edacdf4e14d9d81fb5d35b64
Carlos Pereira <carlos.pereira@tekever.com>
2023-10-19 18:16:26 +0100

    Add PlanCard component

--- src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
+++ src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
@@ -1,42 +1,10 @@
-import { useState } from 'react';
-import { PLANS, Plan } from '../../../constants/plans';
-import getIcon from '../../../utils/getIcon';
+import PlanCard from '../../PlanCard/PlanCard';
 
 import styles from './ContentSelectPlan.module.scss';
 const ContentSelectPlan = () => {
-  const [plansList, setPlansList] = useState<Plan[]>(PLANS)
-
-  const handleSelectPlan = (planId: number) => {
-    const updatedPlansList = plansList.map(plan =>
-      plan.id === planId ? { ...plan, selected: true } : { ...plan, selected: false }
-    );
-    setPlansList(updatedPlansList);
-  }
-  
-
   return (
     <div className={styles.container}>
-      {plansList.map((plan) => {
-        return (
-          <div
-            key={plan.id}
-            role='button'
-            className={
-              plan.selected
-                ? `${styles['plan-card']} ${styles.selected}`
-                : `${styles['plan-card']}`
-            }
-            onClick={() => handleSelectPlan(plan.id)}
-          >
-            <img className={styles['plan-icon']} src={getIcon(plan.planIcon)} alt={`${plan.planName} icon`} />
-            <div className='text-content'>
-              <p className={styles['plan-name']}>{plan.planName}</p>
-              <p className={styles['plan-price']}>{plan.monthPrice}</p>
-              <p className={styles['plan-promotion']}>2 months free</p>
-            </div>
-          </div>
-        );
-      })}
+      <PlanCard />
     </div>
   );
 };
