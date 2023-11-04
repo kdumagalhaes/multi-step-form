5636c413d100c1865d5d0ec8d466bd33ad0f0242
Carlos Pereira <carlos.pereira@tekever.com>
2023-11-04 13:57:05 +0000

    Add summary items

--- src/components/Content/ContentSummary/ContentSummary.tsx
+++ src/components/Content/ContentSummary/ContentSummary.tsx
@@ -1,5 +1,39 @@
+import styles from './ContentSummary.module.scss';
+
 const ContentSummary = () => {
-  return <div>summary</div>;
+  return (
+    <>
+      <div className={styles.summary}>
+        <div className={styles['selected-plan']}>
+          <div className={styles['plan-title']}>
+            <p className={styles['plan-name']}>Arcade (Monthly)</p>
+            <a href='#' className={styles['plan-mode']}>
+              Change
+            </a>
+          </div>
+          <p className={styles['plan-price']}>$9/mo</p>
+        </div>
+        <ul className={styles['item-list']}>
+          <li className={styles.item}>
+            <p className={styles['item-name']}>Online service</p>
+            <p className={styles['item-price']}>+$1/mo</p>
+          </li>
+          <li className={styles.item}>
+            <p className={styles['item-name']}>Online service</p>
+            <p className={styles['item-price']}>+$1/mo</p>
+          </li>
+          <li className={styles.item}>
+            <p className={styles['item-name']}>Online service</p>
+            <p className={styles['item-price']}>+$1/mo</p>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.total}>
+        <p className={styles['total-mode']}>Total (per month)</p>
+        <p className={styles['total-price']}>+$12/mo</p>
+      </div>
+    </>
+  );
 };
 
 export default ContentSummary;
