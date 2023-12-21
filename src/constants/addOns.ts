6d9b5740d41e470e706be60228e893c4ece1e875
Carlos Pereira <carlos.pereira@tekever.com>
2023-12-21 20:40:30 +0000

    Remove add on selected value

--- src/constants/addOns.ts
+++ src/constants/addOns.ts
@@ -4,7 +4,6 @@ export interface AddOns {
   description: string;
   yearPrice: string;
   monthPrice: string;
-  selected: boolean
 }
 
 export const ADD_ONS: AddOns[] = [
@@ -13,23 +12,20 @@ export const ADD_ONS: AddOns[] = [
     title: "Online service",
     description: "Access to multiplayer games",
     monthPrice: "+$1/mo",
-    yearPrice: "+10/yr",
-    selected: false
+    yearPrice: "+10/yr"
   },
   {
     id: "acd1be8c3d4ac2b6ee9cb359caad787d5526f06f",
     title: "Larger storage",
     description: "Extra 1TB of cloud save",
     monthPrice: "+$2/mo",
-    yearPrice: "+$20/yr",
-    selected: false
+    yearPrice: "+$20/yr"
   },
   {
     id: "f9ac14b63a75faf57d8db6f919bfabb2502d273c",
     title: "Customizable profile",
     description: "Custom theme on your profile",
     monthPrice: "+$2/mo",
-    yearPrice: "+$20/yr",
-    selected: false
+    yearPrice: "+$20/yr"
   }
 ];
