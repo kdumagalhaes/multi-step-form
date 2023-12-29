1344ab617b11b924239edb7b92540fe8bfec627e
Carlos Pereira <carlos.pereira@tekever.com>
2023-12-29 19:13:52 +0000

    Add default selected plan type ID && select default plan on render plan screen

--- src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
+++ src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Toggle from "../../Toggle";
 import PlanCard from "../../PlanCard";
-import { PLANS, Plan } from "../../../constants/plans";
+import { DEFAULT_SELECTED_PLAN_TYPE_ID, PLANS, Plan } from "../../../constants/plans";
 import { useAppSelector } from "../../../redux/store";
 import { localStorageKey } from "../../../constants/localStorageKeys";
 
@@ -9,19 +9,28 @@ import styles from "./ContentSelectPlan.module.scss";
 
 const ContentSelectPlan = () => {
   const selectedPlanMode = useAppSelector((state) => state.plan.planMode);
-
+  
   const planMode = localStorage.getItem(localStorageKey.PLAN_MODE);
+  const currentSelectedPlanID = localStorage.getItem(localStorageKey.PLAN_TYPE_ID);
 
   const [plansList, setPlansList] = useState<Plan[]>(PLANS);
   const [isClicked, setIsClicked] = useState(planMode === "yearly");
 
-  const handleSelectPlan = (planID: string) => {
-    const updatedPlansList = plansList.map((plan) =>
-      plan.id === planID  ? { ...plan, selected: true } : { ...plan, selected: false }
-    );
+  const updatePlansList = useCallback((planID: string) => {
+    const updatedPlansList = plansList.map((plan) => 
+    plan.id === planID ? { ...plan, selected: true } : { ...plan, selected: false });
     setPlansList(updatedPlansList);
     localStorage.setItem(localStorageKey.PLAN_TYPE_ID, planID);
-  };
+  }, [plansList]);
+
+  useEffect(() => {
+    currentSelectedPlanID 
+    ? updatePlansList(currentSelectedPlanID)
+    : updatePlansList(DEFAULT_SELECTED_PLAN_TYPE_ID);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentSelectedPlanID]);
+
 
   return (
     <div className={styles.container}>
@@ -37,7 +46,7 @@ const ContentSelectPlan = () => {
               selected={plan.selected}
               yearPrice={plan.yearPrice}
               planMode={selectedPlanMode}
-              handleSelectPlan={() => handleSelectPlan(plan.id)}
+              handleSelectPlan={() => updatePlansList(plan.id)}
             />
           );
         })}
