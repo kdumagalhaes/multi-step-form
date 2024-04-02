40688e887964e7b1d4977873dfb015001421e55f
Carlos Pereira <carlos.pereira@tekever.com>
2024-04-02 21:49:54 +0100

    Add priceConverter util && update prices to number

--- src/constants/plans.ts
+++ src/constants/plans.ts
@@ -13,8 +13,8 @@ export interface Plan {
   id: string;
   planName: PlansNames;
   planIcon: PlansNames;
-  yearPrice: string;
-  monthPrice: string;
+  yearPrice: number;
+  monthPrice: number;
   selected: boolean;
 }
 
@@ -25,24 +25,24 @@ export const PLANS: Plan[] = [
     id: "c16c6aeeed290415006babae333a79ab4d26aba6",
     planName: PlansNames.ARCADE,
     planIcon: PlansNames.ARCADE,
-    yearPrice: "$90/yr",
-    monthPrice: "$9/mo",
+    yearPrice: 90,
+    monthPrice: 9,
     selected: true
   },
   {
     id: "e62854369199d65b6a66cd90fe8203ac4d5be26c",
     planName: PlansNames.ADVANCED,
     planIcon: PlansNames.ADVANCED,
-    yearPrice: "$120/yr",
-    monthPrice: "$12/mo",
+    yearPrice: 120,
+    monthPrice: 12,
     selected: false
   },
   {
     id: "3b40f38967952a3fcd286e1c6b5790d84ff40893",
     planName: PlansNames.PRO,
     planIcon: PlansNames.PRO,
-    yearPrice: "$150/yr",
-    monthPrice: "$15/mo",
+    yearPrice: 150,
+    monthPrice: 15,
     selected: false
   }
 ];
