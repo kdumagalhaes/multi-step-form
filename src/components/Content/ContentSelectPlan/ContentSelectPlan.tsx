64256cc268793e7077fb127da258128463df1fac
Carlos Pereira <carlos.pereira@tekever.com>
2024-04-30 19:03:35 +0100

    Add usePlansList hook in ContentSelectPlan

--- src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
+++ src/components/Content/ContentSelectPlan/ContentSelectPlan.tsx
@@ -1,36 +1,17 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import Toggle from "../../Toggle";
 import PlanCard from "../../PlanCard";
-import { DEFAULT_SELECTED_PLAN_TYPE_ID, PLANS, Plan } from "../../../constants/plans";
 import { useAppSelector } from "../../../redux/store";
+import usePlansList from "../../../hooks/index";
 import { localStorageKey } from "../../../constants/localStorageKeys";
 
 import styles from "./ContentSelectPlan.module.scss";
 
 const ContentSelectPlan = () => {
-  const selectedPlanMode = useAppSelector((state) => state.plan.planMode);
-  
   const planMode = localStorage.getItem(localStorageKey.PLAN_MODE);
-  const currentSelectedPlanID = localStorage.getItem(localStorageKey.PLAN_TYPE_ID);
-
-  const [plansList, setPlansList] = useState<Plan[]>(PLANS);
+  const selectedPlanMode = useAppSelector((state) => state.plan.planMode);
+  const {plansList, updatePlansList} = usePlansList();
   const [isClicked, setIsClicked] = useState(planMode === "yearly");
-
-  const updatePlansList = useCallback((planID: string) => {
-    const updatedPlansList = plansList.map((plan) => 
-    plan.id === planID ? { ...plan, selected: true } : { ...plan, selected: false });
-    setPlansList(updatedPlansList);
-    localStorage.setItem(localStorageKey.PLAN_TYPE_ID, planID);
-  }, [plansList]);
-
-  useEffect(() => {
-    currentSelectedPlanID 
-    ? updatePlansList(currentSelectedPlanID)
-    : updatePlansList(DEFAULT_SELECTED_PLAN_TYPE_ID);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentSelectedPlanID]);
-
 
   return (
     <div className={styles.container}>
