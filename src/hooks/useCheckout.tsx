8426173e5ff0bfc1450e20f7015b5a9c8e34bdbb
Carlos Pereira <carlos.pereira@tekever.com>
2024-05-01 16:32:35 +0100

    Add memoization in checkout to prevent rerenders

--- src/hooks/useCheckout.tsx
+++ src/hooks/useCheckout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ADD_ONS } from "../constants/addOns";
 import { PLANS, PlansModes, PlansNames } from "../constants/plans";
 import { localStorageKey } from "../constants/localStorageKeys";
@@ -22,18 +22,18 @@ const useCheckout = () => {
     planPrice: 0
   });
   const [addOnsList, setAddOnsList] = useState<SelectedAddOns[] | []>([]);
-  const [total, setTotal] = useState(0);
+  const [orderTotal, setOrderTotal] = useState(0);
 
-  const handleCheckout = (planTypeID: string, addOnsIDsList: string[]) => {
-    const selectedPlanDetails = PLANS.find(plan => plan?.id === planTypeID);
-    
+  const handleCheckout = useCallback((planTypeID: string, addOnsIDsList: string[]) => {
+    const selectedPlanDetails = PLANS.find((plan) => plan?.id === planTypeID);
+
     if (!selectedPlanDetails) {
       console.error("Selected plan details not found");
       return;
     }
-  
-    const filteredAddOns = ADD_ONS.filter(addOn => addOnsIDsList.includes(addOn.id));
-  
+
+    const filteredAddOns = ADD_ONS.filter((addOn) => addOnsIDsList.includes(addOn.id));
+
     let planPrice;
     switch (selectedPlan) {
       case PlansModes.MONTHLY:
@@ -46,37 +46,36 @@ const useCheckout = () => {
         console.error("Invalid plan mode");
         return;
     }
-  
+
     setPlanInfo({
       planPrice,
       planName: selectedPlanDetails.planName as PlansNames
     });
-  
-    setAddOnsList(filteredAddOns.map(({ id, monthPrice, yearPrice, title }) => {
-      const price = selectedPlan === PlansModes.MONTHLY ? monthPrice : yearPrice;
-      return { id, price, title };
-    }));
-  };
+
+    setAddOnsList(
+      filteredAddOns.map(({ id, monthPrice, yearPrice, title }) => {
+        const price = selectedPlan === PlansModes.MONTHLY ? monthPrice : yearPrice;
+        return { id, price, title };
+      })
+    );
+  }, [selectedPlan]);
   
 
   useEffect(() => {
     const storedPlanID = localStorage.getItem(localStorageKey.PLAN_TYPE_ID) as string;
     const currentSelectedAddOnsIDs = localStorage.getItem(localStorageKey.ADD_ONS);
-    const storedAddOnsIDs = currentSelectedAddOnsIDs 
-    ? JSON.parse(currentSelectedAddOnsIDs) 
-    : [];
+    const storedAddOnsIDs = currentSelectedAddOnsIDs ? JSON.parse(currentSelectedAddOnsIDs) : [];
 
     handleCheckout(storedPlanID, storedAddOnsIDs);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedPlan]);
+  }, [handleCheckout]);
 
   useEffect(() => {
     const addOnsTotal = addOnsList.reduce((acc, addOn) => acc + addOn.price, 0);
-    setTotal(planInfo.planPrice + addOnsTotal);
+    setOrderTotal(planInfo.planPrice + addOnsTotal);
   }, [planInfo.planPrice, addOnsList]);
 
   return {
-    orderTotal: total,
+    orderTotal,
     selectedPlan,
     addOnsList,
     planInfo
